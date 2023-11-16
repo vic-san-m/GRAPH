0a6ecede8439c0b9bf82a2eb1f8892198b3ed4df
vic-san-m <114695735+vic-san-m@users.noreply.github.com>
2023-11-16 09:37:14 -0600

    Add files via upload
    carga inicial - 26

---
+++ server/app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const express = require('express');
+const routes = require('./routes.js');
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = '<INSERT YOUR CONNECTION STRING HERE>';
+const dbName = "citibike"
+
+
+let app = express();
+
+let allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowCrossDomain);
+
+
+MongoClient.connect(uri, (err, client) => {
+  
+  // Get the citibike database
+  const db = client.db(dbName)
+
+  if (err) {
+    console.error('Unable to connect to database. Is it running on port 27017?');
+    process.exit(1);
+  }
+
+
+
+  app.get('/stations', routes.getStations.bind(null, db));
+  app.get('/stations/statistics', routes.getStationSummary.bind(null, db));
+  app.get('/stations/statistics/:id', routes.getStationStatistics.bind(null, db));
+  app.get('/stations/:id', routes.getStation.bind(null, db));
+  app.get('/bikes', routes.getBike.bind(null, db));
+
+  app.listen(8081, () => console.log("Server listening..."));
+});
+
